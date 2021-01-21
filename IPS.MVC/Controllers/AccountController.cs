7cc3805cd79fc0187a12591ebcdd6e5f3a165f6e
aleiwisdom <aleiwisdom@gmail.com>
2021-01-22 00:00:04 +0800

    添加：Data层，IDbAccess接口，EFCoreAccess(未写具体实现)

---
+++ IPS.MVC/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPS.MVC.Controllers {
+    public class AccountController : Controller {
+        public IActionResult Index() {
+
+            return View();
+        }
+    }
+}
